e13d1880b8240f6de767750f053466ef74f19d06
teiling88 <teiling88@gmail.com>
2026-02-07 13:47:41 +0100

    FIX: disable notices and warnings

--- libs/Smarty_Compiler.class.php
+++ libs/Smarty_Compiler.class.php
@@ -394,6 +394,7 @@ class Smarty_Compiler extends Smarty {
         // put header at the top of the compiled template
         $template_header = "<?php /* Smarty version ".$this->_version.", created on ".date("Y-m-d H:M:S")."\n";
         $template_header .= "         compiled from ".strtr(urlencode($resource_name), array('%2F'=>'/', '%3A'=>':'))." */ ?>\n";
+        $template_header .= "<?php error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING); ?>\n";
 
         /* Emit code to load needed plugins. */
         $this->_plugins_code = '';
