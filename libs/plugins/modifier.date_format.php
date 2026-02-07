ec9e1ead72473c9c1fad4b05368318d801690b0b
teiling88 <teiling88@gmail.com>
2026-02-07 12:31:08 +0100

    compatibility to php8.2

--- libs/plugins/modifier.date_format.php
+++ libs/plugins/modifier.date_format.php
@@ -37,19 +37,10 @@ function smarty_modifier_date_format($string, $format = 'b e, Y', $default_date 
     } else {
         return;
     }
-    if (DIRECTORY_SEPARATOR == '\\') {
-        $_win_from = array('D',       'h', 'n', 'r',          'R',    't', 'T');
-        $_win_to   = array('m/d/y', 'b', "\n", 'I:M:S p', 'H:M', "\t", 'H:M:S');
-        if (strpos($format, 'e') !== false) {
-            $_win_from[] = 'e';
-            $_win_to[]   = sprintf('%\' 2d', date('j', $timestamp));
-        }
-        if (strpos($format, 'l') !== false) {
-            $_win_from[] = 'l';
-            $_win_to[]   = sprintf('%\' 2d', date('h', $timestamp));
-        }
-        $format = str_replace($_win_from, $_win_to, $format);
-    }
+
+    // important fix for old date formats from strftime, which are still used in some templates
+    $format = str_replace('%', '', $format);
+
     return date($format, $timestamp);
 }
 
